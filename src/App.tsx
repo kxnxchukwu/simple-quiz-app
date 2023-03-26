b25048a9781afed3ab76eaaffeca69cab0a2aa0d
Kenechukwu <kenechukwu.ajufo@gmail.com>
2023-03-26 23:33:04 +0100

    Added JumpToQuestionModal Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Quiz } from "./components/Quiz";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
+import JumpToQuestionModal from "./components/Modal";
 
 function App() {
   return (
     <div className="m-1 p-1">
       <NavBar />
       <Quiz />
+      <JumpToQuestionModal />
     </div>
   );
 }
