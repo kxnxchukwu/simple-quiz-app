629fb9d58be835c1f3681841ba24ac94bcdfb514
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-06-23 12:32:26 +0100

    Created Digit Component

---
+++ src/components/Digit.tsx
@@ -0,0 +1,67 @@
+import { ReactElement } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 5px;
+  &: first-child {
+    margin-left: 0;
+  }
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: center; /* Can be changed in the live sample */
+  }
+`;
+
+const DigitContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 0;
+`;
+
+const SingleDigit = styled.span`
+  position: relative;
+  display: flex;
+  flex: 0 1 25%;
+  font-size: 30px;
+  background-color: #404549;
+  border-radius: 5px;
+  padding: 10px 12px;
+  color: white;
+  &:first-child {
+    margin-right: 2px;
+  }
+  &:after {
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    top: 50%;
+    bottom: 50%;
+    width: "100%";
+    height: 2px;
+    background-color: #232323;
+    opacity: 0.4;
+  }
+  @media (max-width: 768px) {
+    font-size: 10px;
+  }
+`;
+
+export interface DigitProps {
+  value: number;
+}
+
+export default function Digit({ value }: DigitProps): ReactElement {
+  const leftDigit = value >= 10 ? value.toString()[0] : "0";
+  const rightDigit = value >= 10 ? value.toString()[1] : value.toString();
+  return (
+    <Container>
+      <DigitContainer>
+        <SingleDigit>{leftDigit}</SingleDigit>
+        <SingleDigit>{rightDigit}</SingleDigit>
+      </DigitContainer>
+    </Container>
+  );
+}
