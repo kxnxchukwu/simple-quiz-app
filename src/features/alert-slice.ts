443116344bbfa7987c45c0713e4f3d508e51673e
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-06-23 12:34:20 +0100

    Updated Alert Slice

--- src/features/alert-slice.ts
+++ src/features/alert-slice.ts
@@ -1,27 +1,27 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { setUserAnswer } from './quiz-slice';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { setUserAnswer } from "./quiz-slice";
 
 interface AlertState {
-    show: boolean;
+  show: boolean;
 }
 
 const initialState: AlertState = {
-    show: false
+  show: false,
 };
 
 export const alertSlice = createSlice({
-    name: 'alert',
-    initialState,
-    reducers: {
-        setShow: (state, action: PayloadAction<{ show: boolean }>) => {
-            state.show = action.payload.show
-        }
+  name: "alert",
+  initialState,
+  reducers: {
+    setShow: (state, action: PayloadAction<{ show: boolean }>) => {
+      state.show = action.payload.show;
     },
-    extraReducers: (builder) => {
-        builder.addCase(setUserAnswer, (state) => {
-            state.show = true
-        })
-    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(setUserAnswer, (state) => {
+      state.show = false;
+    });
+  },
 });
 
 export const { setShow } = alertSlice.actions;
