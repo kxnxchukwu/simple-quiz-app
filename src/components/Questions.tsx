41539d9eb8a3d08d845b747a2f7c20de84aa0557
kxnxchukwu <kenechukwu.ajufo@gmail.com>
2023-06-23 12:37:23 +0100

    Removed redux action props from Question Component

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,9 +1,8 @@
 import React, { useMemo } from "react";
-import { Button } from "react-bootstrap";
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
 import { shuffleArray } from "../utils/shuffle";
 import Explanation from "./Explanation";
+import ActionButtons from "./ActionButtons";
+import OptionContainer from "./OptionsContainer";
 
 export interface QuestionsProps {
   currentQuestion: QuizQuestion;
@@ -11,73 +10,48 @@ export interface QuestionsProps {
   userAnswers: {
     [questionId: number]: number[];
   };
-  handleOptionClick: (option: QuizOption, index: number) => void;
-  handleNextQuestionButtonClick: () => void;
-  handlePrevQuestionButtonClick: () => void;
+  className?: string;
 }
 
 export default function Questions({
   currentQuestion,
   isQuizOver,
   userAnswers,
-  handleOptionClick,
-  handleNextQuestionButtonClick,
-  handlePrevQuestionButtonClick,
+  className,
 }: QuestionsProps): React.ReactElement {
-  const { question, options, explanation, id } = currentQuestion;
-  const active = useSelector((state: RootState) => state.quiz.selectedOption);
+  const [userOption, setUserOption] = React.useState("");
+
+  const onChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserOption(event.target.value);
+  };
+  const { question, options, explanation, id, isMultiChoice } = currentQuestion;
   const shuffledOptions = useMemo(() => shuffleArray(options), [options]);
+
   return (
-    <div className="container-fluid mx-auto mt-2 pt-2">
-      <h2 className="lead h5">
-        {`${id}. `}
-        {question}
-      </h2>
-      <>
+    <div
+      className={`questionContainer card border-0 mt-2 p-2 mx-auto ${
+        className ? "" : "container"
+      }`}
+    >
+      <h4 className="question">{question}</h4>
+      <div className="optionContainer mb-4 row col-12">
         {shuffledOptions.map((option, index) => (
-          <div className="d-grid gap-2" key={option.id}>
-            <Button
-              onClick={() => handleOptionClick(option, index + 1)}
-              disabled={isQuizOver}
-              variant={`${
-                !option.isCorrect && active === index + 1
-                  ? "outline-danger"
-                  : "outline-success"
-              }`}
-              size="lg"
-              className={`m-2 p-2 ${
-                active === index + 1 || (option.isCorrect && !!active)
-                  ? "active"
-                  : ""
-              }`}
-            >
-              {option.label}
-            </Button>
-          </div>
+          <OptionContainer
+            key={`question-${id}-index-${index}-option-${option.id}`}
+            onChangeValue={onChangeValue}
+            userOption={userOption}
+            isQuizOver={isQuizOver}
+            type={isMultiChoice}
+            option={option}
+            currentQuestion={currentQuestion}
+            index={index}
+          />
         ))}
         {explanation && userAnswers[currentQuestion.id] && (
           <Explanation explanation={explanation} />
         )}
-
-        <div className="mb-4 p-4">
-          <Button
-            variant="success"
-            className="float-start"
-            onClick={() => handlePrevQuestionButtonClick()}
-            disabled={id === 1 || isQuizOver}
-          >
-            Prev Question
-          </Button>
-          <Button
-            variant="success"
-            className="float-end"
-            onClick={() => handleNextQuestionButtonClick()}
-            disabled={isQuizOver}
-          >
-            Next Question
-          </Button>
-        </div>
-      </>
+      </div>
+      {className ? <></> : <ActionButtons id={id} />}
     </div>
   );
 }
